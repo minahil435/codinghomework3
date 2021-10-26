7696f94245e91b79148abb66500d7adf828b4fcb
Minahil Shafique <minahal.shafique@gmail.com>
2021-10-26 15:09:10 -0500

    homework completed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from "react";
 
 function App() {
+  const [counter, setCounter] = useState(0);
+  const [dataArray, setDataArray] = useState([]);
+
+
+  function removeButtonClicked() {
+    if (counter === 0) { }
+    else {
+      setCounter(counter - 1)
+      makeStars(counter - 1)
+    }
+  }
+
+  function addButtonClicked() {
+    setCounter(counter + 1)
+    makeStars(counter + 1)
+  }
+
+  function makeStars(counter) {
+    var array = new Array(counter).fill("hi")
+    setDataArray(array)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div id="heading">
+        <div class="bold">LOGO</div>
+        <div>menu</div>
+      </div>
+
+      <center><div id="main">
+        <div><button id="remove" type="submit"
+          onClick={removeButtonClicked} >remove 1</button></div>
+        <div id="counterDiv" class="bold largeText">{counter}</div>
+        <div><button id="add" type="submit"
+          onClick={addButtonClicked}>Add 1</button></div>
+      </div></center>
+
+      <center>
+        <div id="starDiv">
+          {dataArray.map(() => {
+            return (
+              <div className="starBox">
+                <img className="starBoxImage" src="./starLogo.png" />
+              </div>
+            )
+          })}
+        </div>
+      </center>
     </div>
   );
 }
